b0972770a00e52c72a3ef3dcca1e546e48e6c549
Luke Green <me@lukegreen.xyz>
2026-09-14 18:51:54 -0500

    Remove debug

--- src/config/config.ts
+++ src/config/config.ts
@@ -44,15 +44,6 @@ export const registerConfig = () => {
     !dbPass ||
     !logMinLevel
   ) {
-    console.error(
-      dbHost,
-      dbPort,
-      dbPass,
-      dbDatabase,
-      dbUser,
-      dbPass,
-      logMinLevel,
-    );
     throw new Error("Malformed config");
   }
 
