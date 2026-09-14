1b70decaea72c7e48e7443ffa429ebb2aaaa9ed5
Luke Green <me@lukegreen.xyz>
2026-09-13 20:27:41 -0500

    rewrite in typescript

--- index.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-declare(strict_types=1);
-
-use App\Main;
-
-require __DIR__ . '/vendor/autoload.php';
-
-(new Main())();
--- src/Framework/Config/Exceptions/RequiredConfigValueNotFoundException.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-declare(strict_types=1);
-
-namespace App\Framework\Config\Exceptions;
-
-class RequiredConfigValueNotFoundException extends \Exception
-{
-}
